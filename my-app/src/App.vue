c98f3b78ed999e0c6db22bd0bbfccfc9aa16cbeb
zjy <18144091761@163.com>
2026-08-18 10:22:44 +0800

    import.meta 放在script

--- my-app/src/App.vue
+++ my-app/src/App.vue
@@ -4,9 +4,10 @@ import { isNumber } from 'my-tools'
 
 console.log('isNumber', isNumber(123));
 console.log('isNumber', isNumber('123'));
+const appEnv = import.meta.env.VITE_ENV
 </script>
 
 <template>
   <HelloWorld msg="my-app" />
-  <p>当前环境：{{ import.meta.env.VITE_ENV }}</p>
+  <p>当前环境：{{ appEnv }}</p>
 </template>
