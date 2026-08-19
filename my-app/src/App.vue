5eee0e90962579931716fb287cb8190d690c8915
zjy <18144091761@163.com>
2026-08-19 14:09:35 +0800

    一些修改

--- my-app/src/App.vue
+++ my-app/src/App.vue
@@ -5,10 +5,11 @@ import { isNumber } from 'my-tools'
 console.log('isNumber', isNumber(123));
 console.log('isNumber', isNumber('123'));
 const appEnv = import.meta.env.VITE_ENV
+const flag = false
 </script>
 
 <template>
   <h1>无敌鲍哥</h1>
-  <HelloWorld msg=" " />
+  <HelloWorld v-if="flag" msg="HelloWorld" />
   <p>当前环境：{{ appEnv }}</p>
 </template>
